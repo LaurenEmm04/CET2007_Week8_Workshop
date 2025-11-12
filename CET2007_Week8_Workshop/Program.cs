384102ce2b169c437f313c54a4edc21eea778231
lauren em <bi76os@student.sunderland.ac.uk>
2025-11-12 15:56:34 +0000

    Completed exersise 1

--- CET2007_Week8_Workshop/Program.cs
+++ CET2007_Week8_Workshop/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace CET2007w8
+{
+    ///<summary>
+    /// Patient class
+    /// </summary>
+    /// 
+    public class Patient
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        int Severity { get; set; }
+        bool IsRead { get; set; }
+
+        public Patient(int id, string name, int severity, bool isread)
+        {
+            ID = id;
+            Name = name;
+            Severity = severity;
+            IsRead = isread;
+        }
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Severity: {Severity}";
+        }
+    }
+    /// <summary>
+    /// main
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Patient> patients = new List<Patient>();
+
+            patients.Add(new Patient(1, "Geoff Phishy", 2, true));
+            patients.Add(new Patient(2, "Harvey Hagan", 10, true));
+            patients.Add(new Patient(3, "Jack Fawcett", 10, true));
+
+        }
+    }
+
+}
